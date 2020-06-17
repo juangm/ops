f3c675f9bd359677ee91b1d8f5750fe027fc9cd9
juangm <juangarciamarinblog@gmail.com>
2020-06-17 15:57:38 +0200

    Modify script to download chromedriver

--- scripts/updateChromedriver.sh
+++ scripts/updateChromedriver.sh
@@ -1,6 +1,13 @@
-CHROMEDRIVER_VERSION=`curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
-    curl https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip --output chromedriver_linux64.zip && \
-    unzip chromedriver_linux64.zip && \
-    rm chromedriver_linux64.zip && \
-    mv -f chromedriver /usr/bin/chromedriver && \
-    chmod 0777 /usr/bin/chromedriver
+#!/bin/bash
+
+# Get the latest version from the API
+CHROMEDRIVER_VERSION=`curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
+
+# Dowload the package (linux version)
+curl https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip --output chromedriver_linux64.zip
+
+# Unzip the package and configure
+unzip chromedriver_linux64.zip
+rm chromedriver_linux64.zip
+mv -f chromedriver /usr/bin/chromedriver
+chmod 0777 /usr/bin/chromedriver
