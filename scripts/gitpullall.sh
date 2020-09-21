d7071e18767ae1c79b69ffe16bd86d8c20963cc8
juangm <juangarciamarinblog@gmail.com>
2020-09-21 12:40:30 +0200

    Add log in gitpullall script

--- scripts/gitpullall.sh
+++ scripts/gitpullall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ## Search in subdirectories and execute git pull
-find . -type d -depth 1 -exec bash -c "cd $PWD/{} && git pull" \;
+find . -type d -depth 1 -exec bash -c "echo 'Git Pull in -> {}' && cd $PWD/{} && git pull" \;
