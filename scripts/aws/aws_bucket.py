76d437e07b40c2ee1d249079eaab8a8bb986a272
juangm <juangarciamarinblog@gmail.com>
2020-09-29 10:10:35 +0200

    Refactor aws_bucket.py

--- scripts/aws/aws_bucket.py
+++ scripts/aws/aws_bucket.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+import argparse
 import boto3
-import json
-import os
 import progressbar
-
+import os
 
 def create_session(profile):
     session = boto3.session.Session(profile_name=profile)
@@ -13,8 +12,32 @@ def upload_progress(chunk):
     up_progress.update(up_progress.currval + chunk)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('env',
+                        metavar='path',
+                        type=str,
+                        help='Environment to select DEV or TEST')
+
+    args = parser.parse_args()
+    environment = args.env
+
     # Create session and get variables
-    config = json.loads(open("scripts/aws/s3_bucket_config.json").read())
+    if environment == 'dev':
+        config = {
+            "AWS_PROFILE": "Quality",
+            "BUCKET_NAME": "qa-jenkins-s3-iwb",
+            "BUCKET_MAIN_FOLDER": "report/Automated_Dev_DEVELOP_BRANCH/1",
+            "BUCKET_DOWNLOAD_FOLDER": "report/Automated_Dev_DEVELOP_BRANCH/{}",
+            "OUTPUT_FOLDER": "/reports/Dev/{}"
+        }
+    else:
+        config = {
+            "AWS_PROFILE": "Quality",
+            "BUCKET_NAME": "qa-jenkins-s3-iwb",
+            "BUCKET_MAIN_FOLDER": "report/Automated_Test_DEVELOP_BRANCH/3",
+            "BUCKET_DOWNLOAD_FOLDER": "report/Automated_Test_DEVELOP_BRANCH/{}",
+            "OUTPUT_FOLDER": "/reports/Test/{}"
+        } 
     s3_client = create_session(config["AWS_PROFILE"])
 
     latest_builds = s3_client.list_objects_v2(
@@ -34,7 +57,7 @@ if __name__ == "__main__":
         Bucket=config["BUCKET_NAME"],
         Prefix=config["BUCKET_DOWNLOAD_FOLDER"].format(build_number))
     
-    object_path = os.getcwd() + "/report/{}".format(build_number)
+    object_path = os.getcwd() + config["OUTPUT_FOLDER"].format(build_number)
     if not os.path.exists(object_path):
         try:
             os.makedirs(object_path)
